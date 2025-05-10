0787811081a15362e86c1918eb97a22fe7fa5014
Кирилл Расторгуев <kirillrastorguev5@gmail.com>
2025-05-10 19:28:19 +0300

    Доработки

--- src/hh.py
+++ src/hh.py
@@ -5,19 +5,19 @@ from src.parser import Parser
 
 
 class HeadHunterAPI(Parser):
-    """
-    Конкретная реализация VacancyAPI для платформы hh.ru
-    """
-
     def __init__(self):
         self.__url = "https://api.hh.ru/vacancies"
         self.__headers = {"User-Agent": "HH-User-Agent"}
         self.__params = {"text": "", "page": 0, "per_page": 50}
 
+    def __handle_response(self, response: requests.Response) -> list[dict]:
+        try:
+            response.raise_for_status()
+            return response.json().get("items", [])
+        except (RequestException, ValueError):
+            return []
+
     def __connect(self, keyword: str) -> list[dict]:
-        """
-        Приватный метод для запроса к API hh.ru
-        """
         self.__params["text"] = keyword
         self.__params["page"] = 0
         all_vacancies = []
@@ -25,20 +25,17 @@ class HeadHunterAPI(Parser):
         for _ in range(5):
             try:
                 response = requests.get(
-                    self.__url, headers=self.__headers, params=self.__params
+                    self.__url, headers=self.__headers, params=self.__params, timeout=10
                 )
-                response.raise_for_status()
-                data = response.json().get("items", [])
+                data = self.__handle_response(response)
+                if not data:
+                    break
                 all_vacancies.extend(data)
                 self.__params["page"] += 1
             except RequestException:
-                return []
-            except ValueError:
-                return []
+                break
+
         return all_vacancies
 
     def load_vacancies(self, keyword: str) -> list[dict]:
-        """
-        Публичный метод
-        """
         return self.__connect(keyword)
